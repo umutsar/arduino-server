36650709596df178c19abf5cdb7c5591e1b00d7c
Umut Şar <contact@umutsar.net>
2023-11-11 00:24:27 +0300

    Update windows.py

--- radar-server/windows.py
+++ radar-server/windows.py
@@ -2,7 +2,6 @@ import serial
 import asyncio
 import websockets
 
-# Windows'ta seri port numarasını belirtmek için COMx kullanılır.
 ser = serial.Serial('COM3', 9600)
 
 async def server(websocket, path):
@@ -13,7 +12,6 @@ async def server(websocket, path):
     except websockets.exceptions.ConnectionClosed:
         pass
 
-# IP adresi olarak "localhost" ya da "127.0.0.1" kullanabilirsiniz.
 start_server = websockets.serve(server, "127.0.0.1", 8766)
 
 asyncio.get_event_loop().run_until_complete(start_server)
